3227eb057cea61ab1748a72aabdb3cf27e3347a6
Alpesh-261 <alpeshsodha2604@gmail.com>
2025-06-03 22:25:37 +0530

    header section of chef claude

---
+++ chef-claude/src/Components/Header.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Header() {
+  return (
+    <header>
+      <img src="./src/assets/chef-claude-icon.png" alt="Chef Claude icon" />
+      <h1>Chef Claude</h1>
+    </header>
+  )
+}
