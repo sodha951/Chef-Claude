feb09b5933162c923fb52c04d89c4b3806077d2a
Alpesh-261 <alpeshsodha2604@gmail.com>
2025-06-03 22:50:30 +0530

    Add ingredient menu

---
+++ chef-claude/src/Components/Main.jsx
@@ -0,0 +1,17 @@
+
+
+export default function Main () {
+  return (
+    <main>
+      <form className="add-ingredient-form">
+        <input
+          type="text"
+          placeholder="e.g. oregano"
+          aria-label="Add ingrediennt" 
+        />
+        <button>Add Ingredient</button>
+
+      </form>
+    </main>
+  )
+}
